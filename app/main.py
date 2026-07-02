298f34013b977886d6e826c2a88e5d24ab1e29cc
Ivan Podgurskiy <ivan.podgurskiy@gmail.com>
2026-07-02 14:37:27 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import sys
 
+available_commands = ["type", "echo", "exit"]
+
 
 def main():
     while True:
@@ -10,13 +12,21 @@ def main():
 
         # split first word and rest from the input
         command, args = (whole_input.split(" ", maxsplit=1) + [""])[:2]
-    
-        if command == "exit":
-            break
-        elif command == "echo":
-            print(args)
+
+        if command in available_commands:
+            match command:
+                case "type":
+                    if args in available_commands:
+                        print(f"{args} is a shell builtin")
+                    else:
+                        print(f"{args}: not found")
+                case "echo":
+                    print(args)
+                case "exit":
+                    break
         else:
             print(f"{command}: command not found")
+
 
 if __name__ == "__main__":
     main()
